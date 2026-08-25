8366fb11aacdebb3b6e31e7e322cc3a5e6e28c54
Pranjal Slathia <pranjalslathia@protonmail.com>
2026-08-25 15:57:58 +0530

    Update problem8.cpp
    Updated the program to reverse names in a vector instead of finding max values.

--- problem8.cpp
+++ problem8.cpp
@@ -1,16 +1,24 @@
 #include <iostream>
 #include <vector>
+#include <string>
 
 int main() {
-    std::vector<int> numbers = {1, 2, 3, 4, 5, 6, 7};
-    int *reverse = new int[numbers.size()]
-    for (int i = 0; i < numbers.size(); i++){
-        if (max < numbers[i]){
-            max_2 = max;
-            max = numbers[i];
-            
-        }
+    std::string name, buffer;
+    std::vector<std::string> names = {"daku", "professor", "billusanda", "hergoodboyy"};
+    for (int i = 0; i < names.size(); i++){
+       name = names[i];
+       buffer = "";
+       for (int i = name.length() - 1; i >= 0 ; i--){
+            buffer += name[i];
+       }
+       names[i] = buffer;
+
     }
-    std::cout << "Max: " << max << " | Second Max: " << max_2 << std::endl;
+    std::cout << "{";
+    for (int i = 0; i < names.size(); i++){
+        std::cout << names[i] << ", ";
+    }
+    std::cout << "}";
+    
     return 0;
 }
